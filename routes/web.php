f87a09304b17e5a1c9f460d741b383b6f3ce8a95
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-06-19 17:13:28 +0300

    8. Laravel 8 Multi Auth Part 1

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+Route::group(['perfix'=>'admin','middleware'=>['admin:admin']],function(){
+    Route::get('/login', [AdminController::class,'loginForm']);
+    Route::post('/login', [AdminController::class,'store'])->name('admin.login');
+});
+
+
+Route::middleware(['auth:sanctum,admin',config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::middleware(['auth:sanctum,web',config('jetstream.auth_session'),'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
