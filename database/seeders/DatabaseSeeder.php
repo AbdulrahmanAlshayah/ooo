b343629b7a5eba3dfcc07d48f58f229e49865e22
Abdulrhman-Coder <abdulrhmanalshayay@gmail.com>
2022-06-19 12:45:54 +0300

    6. Setup Admin Table and Seed Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\Admin::factory()->create();
     }
+//     لتشغيل ال  seeder الذي يتحكم بال factory نكتب php artisan migrate --seed
+// اما لأنشاء واحد خاص فيك php artisan make:seeder AdminSeed
 }
